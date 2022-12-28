52a5d31d9fae7aebea4192e6b7993cd3c615c4cc
Fernando de Paz <aynowell@gmail.com>
2022-12-27 18:01:47 -0800

    started creating title screen

--- Assets/gamemanager.cs
+++ Assets/gamemanager.cs
@@ -4,16 +4,37 @@ using UnityEngine;
 
 public class gamemanager : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+	public GameObject enemyPrefab;
+
+	float spawnDistance = 12f;
+
+	float enemyRate = 5;
+	float nextEnemy = 1;
+
+	private void Start()
+	{
+	}
+	// Update is called once per frame
+	void Update()
+	{
+
+		nextEnemy -= Time.deltaTime;
+
+		if (nextEnemy <= 0)
+		{
+			nextEnemy = enemyRate;
+			enemyRate *= 0.9f;
+			if (enemyRate < 2)
+				enemyRate = 2;
+
+			Vector3 offset = Random.onUnitSphere;
+
+			offset.z = 0;
+
+			offset = offset.normalized * spawnDistance;
+
+			Instantiate(enemyPrefab, transform.position + offset, Quaternion.identity);
+		}
+	}
 }
